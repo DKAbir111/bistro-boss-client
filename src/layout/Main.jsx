46989dbc9e27020712051adcea00a834b959c43d
Darun Karas <darunkaras@Daruns-MacBook-Air.local>
2025-01-03 23:01:26 +0600

    signup section design done

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,13 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import NavBar from "../components/shared/NavBar";
 import Footer from "../components/shared/Footer";
 
 export default function Main() {
+    const location = useLocation()
     return (
         <div className="max-w-screen-2xl mx-auto">
-            <NavBar />
+            {location.pathname === '/login' || location.pathname === '/signup' || <NavBar />}
             <Outlet />
-            <Footer />
+            {location.pathname === '/login' || location.pathname === '/signup' || <Footer />}
+
         </div>
     )
 }
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from 'react';
 import bgImage from '../../assets/others/authentication.png'
 import authImage from '../../assets/others/authentication2.png'
 import { loadCaptchaEnginge, LoadCanvasTemplateNoReload, validateCaptcha } from 'react-simple-captcha';
+import DynamicTitle from '../../components/shared/DynamicTitle';
+import { FaFacebook, FaGithub, FaGoogle } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 export default function Login() {
 
     const [relode, setRelode] = useState(true)
@@ -19,7 +22,6 @@ export default function Login() {
         }
         else {
             setValid(false)
-            e.target.reset()
         }
     }
     return (
@@ -28,45 +30,61 @@ export default function Login() {
             style={{
                 backgroundImage: `url(${bgImage})`,
             }}>
-            <div className="hero-content text-center flex flex-col md:flex-row max-w-screen-lg mx-auto border py-20 shadow-lg gap-10 font-inter text-black">
+            <DynamicTitle title={'BistroBoss | Login'} />
+            <div className="hero-content mt-20 text-center flex flex-col lg:flex-row max-w-screen-xl mx-auto border py-20 shadow-lg lg:gap-10 font-inter text-black">
 
                 <img src={authImage} alt="" className='md:w-1/2' />
 
-                <form className="card-body">
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Email</span>
-                        </label>
-                        <input type="email" placeholder="email" className="input input-bordered" required />
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Password</span>
-                        </label>
-                        <input type="password" placeholder="password" className="input input-bordered" required />
 
-                    </div>
+                <div className="card-body w-full">
+                    <form >
+                        <h2 className='text-2xl font-bold'>Login</h2>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" placeholder="password" className="input input-bordered" required />
 
-                    {/* captcha */}
-                    <div className='mt-3 input input-bordered flex items-center'>
-                        <LoadCanvasTemplateNoReload />
-                    </div>
+                        </div>
+
+                        {/* captcha */}
+                        <div className='mt-6 input input-bordered flex items-center'>
+                            <LoadCanvasTemplateNoReload />
+                        </div>
+                        <div className='my-2'>
+                            <span onClick={() => setRelode(!relode)} className='text-blue-600 text-sm cursor-pointer'>Reload Captcha</span>
+                        </div>
+                        <div className="form-control">
+                            <input type="text" onChange={vadidateCaptcha} placeholder="Type here" className="input input-bordered" required />
+                        </div>
+
+                        {/* submit button */}
+                        <div className="form-control mt-6">
+                            <button className={`btn bg-[#D1A054B3] hover:bg-[#D1A054] text-white ${valid ? '' : 'btn-disabled'}`}>Sign in</button>
+                        </div>
+                        <label className="flex justify-center mt-3">
+                            <a href="#" className="text-[#D1A054] text-sm ">New here? <Link to={'/signup'} className='font-bold hover:underline'>Create a New Account</Link></a>
+                        </label>
+                    </form>
                     <div>
-                        <span onClick={() => setRelode(!relode)} className='text-blue-600 text-sm cursor-pointer'>Reload Captcha</span>
+                        <label className="label flex justify-center flex-col items-center">
+                            <span className="label-text text-[#444444] font-semibold">Or sign in with</span>
+                            <div className='flex gap-5 mt-2'>
+                                <span className='btn btn-circle text-lg  btn-outline border-[#444444] hover:bg-[#444444]'> <FaGoogle /> </span>
+                                <span className='btn btn-circle text-lg btn-outline border-[#444444] hover:bg-[#444444]'> <FaGithub /></span>
+                                <span className='btn btn-circle text-lg btn-outline border-[#444444] hover:bg-[#444444]'> <FaFacebook /></span>
+                            </div>
+                        </label>
                     </div>
-                    <div className="form-control">
-                        <input type="text" onChange={vadidateCaptcha} placeholder="Type here" className="input input-bordered" required />
-                    </div>
-
-                    {/* submit button */}
-                    <div className="form-control mt-6">
-                        <button className={`btn bg-[#D1A054B3] hover:bg-[#D1A054] text-white ${valid ? '' : 'btn-disabled'}`}>Sign in</button>
-                    </div>
-                    <label className="flex justify-center">
-                        <a href="#" className="text-[#D1A054] text-sm ">New here? <span className='font-bold hover:underline'>Create a New Account</span></a>
-                    </label>
-                </form>
+                </div>
             </div>
-        </div >
+        </div>
+
     )
 }
