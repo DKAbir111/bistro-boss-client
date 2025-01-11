7dd79eddb5f138e24eb5b4fbf15f2d2878d5bdbc
Darun Karas <darunkaras@Daruns-MacBook-Air.local>
2025-01-11 12:45:21 +0600

    use react hook form for add item

--- src/pages/Dashboard/Admin/AddItem.jsx
+++ src/pages/Dashboard/Admin/AddItem.jsx
@@ -1,19 +1,32 @@
 import { ImSpoonKnife } from "react-icons/im";
 import SectionTitle from "../../../components/shared/SectionTitle";
-
+import { useForm } from "react-hook-form"
 
 export default function AddItem() {
+    const {
+        register,
+        handleSubmit,
+        // watch,
+        formState: { errors },
+    } = useForm()
+
+    // submit function
+
+    const onSubmit = (data) => console.log(data)
+    // console.log(watch("name"))
+
     return (
         <section className="font-inter pb-10">
             <SectionTitle heading={'CONTACT FORM'} subHeading={'Send Us a Message'} />
             <div className="max-w-screen-lg  mx-auto bg-base-200">
-                <form className="card-body md:grid grid-cols-2 gap-x-10">
+                <form className="card-body md:grid grid-cols-2 gap-x-10" onSubmit={handleSubmit(onSubmit)}>
                     {/* price */}
                     <div className="form-control col-span-2">
                         <label className="label">
                             <span className="label-text">Recipe Name*</span>
                         </label>
-                        <input type="text" placeholder="Recipe name" className="input input-bordered" required />
+                        <input type="text" {...register("name", { required: true })} placeholder="Recipe name" className="input input-bordered" required />
+                        {errors.name && <span className="text-sm text-red-500 mt-1">This field is required</span>}
                     </div>
 
                     {/* category */}
@@ -21,7 +34,7 @@ export default function AddItem() {
                         <div className="label">
                             <span className="label-text">Category*</span>
                         </div>
-                        <select className="select select-bordered">
+                        <select className="select select-bordered" {...register("category", { required: true })}>
                             <option disabled selected>Category</option>
                             <option value={'pizza'}>Pizza</option>
                             <option value={'salad'}>Salad</option>
@@ -29,6 +42,7 @@ export default function AddItem() {
                             <option value={'drinks'}>Drinks</option>
                             <option value={'dessert'}>Desserts</option>
                         </select>
+                        {errors.category && <span className="text-sm text-red-500 mt-1">This field is required</span>}
                     </label>
 
                     {/* price */}
@@ -36,7 +50,8 @@ export default function AddItem() {
                         <label className="label">
                             <span className="label-text">Price*</span>
                         </label>
-                        <input type="number" placeholder="Price" className="input input-bordered" required />
+                        <input type="number" {...register("price", { required: true })} placeholder="Price" className="input input-bordered" required />
+                        {errors.price && <span className="text-sm text-red-500 mt-1">This field is required</span>}
                     </div>
 
 
@@ -46,16 +61,19 @@ export default function AddItem() {
                             <span className="label-text">Recipe Details*</span>
                         </label>
                         <textarea
+                            {...register("details", { required: true })}
                             placeholder="Recipe Details"
                             className="textarea textarea-bordered columns-4"
                             rows="6"
                             required
                         ></textarea>
+                        {errors.details && <span className="text-sm text-red-500 mt-1">This field is required</span>}
                     </div>
 
                     {/* image */}
                     <div className="form-control col-span-2 mt-4">
-                        <input type="file" />
+                        <input type="file" {...register("image", { required: true })} />
+                        {errors.image && <span className="text-sm text-red-500 mt-1">This field is required</span>}
                     </div>
 
                     {/* submit button */}
